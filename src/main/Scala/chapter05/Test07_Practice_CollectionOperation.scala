055eedd30425520f12ea5c3e7148ae62b5ce7fa1
we <614935302@qq.com>
2022-07-13 16:51:52 +0800

    指令代码

--- src/main/Scala/chapter05/Test07_Practice_CollectionOperation.scala
+++ src/main/Scala/chapter05/Test07_Practice_CollectionOperation.scala
@@ -5,12 +5,16 @@ package chapter05
  * @date 2022-07-13 14:01
  */
 object Test07_Practice_CollectionOperation {
+
   def main(args: Array[String]): Unit = {
 
-    //
     def arrayOperation(array: Array[Int],op: Int=>Int):Array[Int] = {
       for (elem<-array) yield op(elem)
     }
+
+
   }
+
+
 
 }
