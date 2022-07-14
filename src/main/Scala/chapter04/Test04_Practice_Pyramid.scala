3ce68c373ab5e448abc0443f826ff76a6638eb04
we <614935302@qq.com>
2022-07-14 16:33:11 +0800

    金字塔输出

--- src/main/Scala/chapter04/Test04_Practice_Pyramid.scala
+++ src/main/Scala/chapter04/Test04_Practice_Pyramid.scala
@@ -8,22 +8,35 @@ object Test04_Practice_Pyramid {
 
   /**
    * 金字塔菱形
+   *
    * @param args
    */
 
   def main(args: Array[String]): Unit = {
 
-    for (i <- 1 to 9){
-      val stars = 2 * i - 1
-      val spaces = 9 - i
+    //    for (i <- 1 to 9){
+    //      val stars = 2 * i - 1
+    //      val spaces = 9 - i
+    //      println(" " * spaces + "*" * stars)
+    //    }
+    //
+    //    for (i <- 1 to 9; stars = 2 * i - 1; spaces = 9 - i){
+    //      println(" " * spaces + "*" * stars)
+    //    }
+
+    for {i <- 1 to 9
+         stars = 2 * i - 1
+         spaces = 9 - i
+         }
+    {
       println(" " * spaces + "*" * stars)
     }
 
-    for (i <- 1 to 9; stars = 2 * i - 1; spaces = 9 - i){
-      println(" " * spaces + "*" * stars)
-    }
 
-    for (stars <- 1 to 17 by 2; spaces = (17 - stars) / 2){
+    for {stars <- 1 to 17 by 2
+         spaces = (17 - stars) / 2
+         }
+    {
       println(" " * spaces + "*" * stars)
     }
 
